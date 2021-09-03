7dae38b902474297cf554c504f0d4dcdd37ad1ee
Angel Gutierrez Monola <gutierrez.monola@gmail.com>
2021-09-03 09:09:16 -0500

    Update php container for Docker file

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>HYTORC | Encuesta de Satisfacción</title>
+	<link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+	<header>
+		<img src="" alt="">
+	</header>
+	<div class="main-content">
+
+	</div>
+	<footer>
+		<div class="row">
+			<div class="col">
+				<div class="social-icons">
+					<ul>
+						<li><a href="#"> <img src="./assets/img/worldwide.png" alt=""> </a> </li>
+						<li><a href="#"> <img src="./assets/img/facebook-logo.png" alt=""> </a> </li>
+						<li><a href="#"> <img src="./assets/img/instagram.png" alt=""> </a> </li>
+						<li><a href="#"> <img src="./assets/img/linkedin.png" alt=""> </a> </li>
+						<li><a href="#"> <img src="./assets/img/twitter.png" alt=""> </a> </li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
